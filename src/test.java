9d4896ffb8c28d2bfb4ee2cc939bc50d55b9c2b1
belong1020 <840451562@qq.com>
2016-12-01 23:49:45 +0800

    pedigree
    2016.12.1

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 
-import Jama.*;
+import Jama.Matrix;
 
 public class test {
 
@@ -8,16 +8,33 @@ public class test {
 		String str = "123456";
 		int i=123456;
 		
-		System.out.println(str.getBytes().length);
-		System.out.println(String.valueOf(i).length());
+		//System.out.println(str.getBytes().length);
+		//System.out.println(String.valueOf(i).length());
 		
 		double[][] a={
 		{1,2,3},{4,5,6}
 		};
 		double[][] b={
-		{1,2,3},{4,5,6}
+		{1,2,3},{4,5,6},
+		{7,8,9},{10,11,12}
 		};
-		double[][] c = solve(a,b);
+		Matrix m1 = new Matrix(a);
+		Matrix m2 = new Matrix(b);
+		Matrix m3 = null ;
+		Matrix m4 = Matrix.random(3,1);
+		
+		Matrix x = m2.inverse();
+		
+		double[][] cvalue = x.getArray();
+		
+		
+		
+		for(int k=0; k<cvalue.length; k++ ){
+			for(int j=0; j<cvalue[0].length; j++){
+				System.out.print(cvalue[k][j]+" ");
+				
+			}System.out.println();
+		}
 		
 		
 	}
